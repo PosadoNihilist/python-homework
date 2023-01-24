d38648cf0babfac25d8234e22b3d068d000f8a51
PosadoNihilist <121141109+PosadoNihilist@users.noreply.github.com>
2023-01-24 15:55:06 +0800

    Update task1_test.py
    fixed task1 test

--- Week_1_HW/task1/task1_test.py
+++ Week_1_HW/task1/task1_test.py
@@ -4,7 +4,7 @@ from task1 import *
 class Test(unittest.TestCase):
     def test_sorting(self):
         test_data = "1. Котов Алексей: 3\n2. Белова Юлия: 3\n3. Шахвалиева Юлиана: 4"
-        desired_data = "name,grade\nБелова Юлия,3\nКотов Алексей,3\nШахвалиева Юлиана,4\n"
+        desired_data = "name,grade\nБелова Юлия,3\nКотов Алексей,3\nШахвалиева Юлиана,4"
         result = format_data(test_data)
         self.assertEqual(result, desired_data)
 
