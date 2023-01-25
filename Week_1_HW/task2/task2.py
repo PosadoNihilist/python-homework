3453d0bc205d6170f15a5e26c6fa871aebe1f2a5
PosadoNihilist <121141109+PosadoNihilist@users.noreply.github.com>
2023-01-25 10:12:21 +0800

    Improved task2

--- Week_1_HW/task2/task2.py
+++ Week_1_HW/task2/task2.py
@@ -1,3 +1,5 @@
+left_section = "KP2.2-"
+
 def read_data(file_name):
     file = open(file_name)
     content = file.read()
@@ -10,29 +12,19 @@ def write_data(file_name, data):
     
 def format_data(text):
     lines = text.split('\n')
-    complete_dict = {}
-    number_list = []
+    indexes = []
+    rows = []
     for line in lines:
-        three_vals = line.split("-")
-        A = three_vals[0]
-        B = three_vals[1].strip(" ")
-        location = three_vals[2].strip(" ")
-        AandB = A+"-"+B
-        complete_dict.update({AandB: location})
-        number_list.append(float(B))
-    final_output = ""
-    for number in sorted(number_list):
-        template = '{} - {}\n'
-        if number.is_integer():
-            key_number = str(int(number))
-        else:
-            key_number = str(number)
-        check_if_left = key_number.split(".")
-        if check_if_left[0] == "1":
-            key_number = "0"+key_number
-        key = "KP2.2-" + key_number
-        final_output += template.format(key, complete_dict.get(key))
-    return str(final_output)
+        line = line.replace(left_section, "")
+        indexes.append(float(line.split(" - ")[0]))
+        rows.append(line)
+    index = list(zip(indexes, rows))
+    index = sorted(index)
+    result = ''
+    for elem in index:
+        result = result + elem[1] + '\n'
+    return result
+
 text = read_data("input.txt")
 out = format_data(text)
 write_data('output.txt', out)
